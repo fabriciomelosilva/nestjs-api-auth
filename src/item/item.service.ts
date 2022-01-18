f1200682f89c4fc431055398b81a8063b4fed958
Fabrício Melo <fabrciomelosilva@gmail.com>
2022-01-18 08:20:22 -0300

    configure service and controllers methods

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
+import { Item } from './entities/item.entity';
 
 @Injectable()
 export class ItemService {
+  constructor(
+    @InjectRepository(Item) private readonly repository: Repository<Item>,
+  ) {}
+
   create(createItemDto: CreateItemDto) {
-    return 'This action adds a new item';
+    const item = this.repository.create(createItemDto);
+    return this.repository.save(item);
   }
 
   findAll() {
-    return `This action returns all item`;
+    return this.repository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  findOne(id: string) {
+    return this.repository.findOne(id);
   }
 
-  update(id: number, updateItemDto: UpdateItemDto) {
-    return `This action updates a #${id} item`;
+  async update(id: string, updateItemDto: UpdateItemDto): Promise<Item> {
+    const item = await this.repository.preload({
+      id: id,
+      ...updateItemDto,
+    });
+    if (!item) {
+      throw new NotFoundException(`Item ${id} not found`);
+    }
+    return this.repository.save(item);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} item`;
+  async remove(id: string) {
+    const item = await this.findOne(id);
+    return this.repository.remove(item);
   }
 }
